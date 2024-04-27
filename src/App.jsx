1bad9f671eb012c65faa039e12fda78d87ddea69
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-27 08:25:13 +0530

    state in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css'
 
+// we need to define state and component
+
+import { useState } from 'react';
+
+// this is my component
 function App() {
+  // this is my state
+  const [count, setCount] = useState(0);
+  
+  function onClickHandler () {
+    setCount(count + 1);
+  }
+
   return (
     <div>
-      Hi there
+      <button onClick={onClickHandler}>counter: {count}</button>
     </div>
   )
 }
