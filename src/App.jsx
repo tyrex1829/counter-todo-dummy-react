b3ad0c44adffbe5c229437d29984db7874229ad1
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-27 08:32:49 +0530

    button component

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,24 @@ function App() {
   // this is my state
   const [count, setCount] = useState(0);
   
-  function onClickHandler () {
-    setCount(count + 1);
-  }
-
   return (
     <div>
-      <button onClick={onClickHandler}>counter: {count}</button>
+      <CustomButton count={count} setCount={setCount} />
     </div>
+  )
+}
+
+// component
+function CustomButton (props) {
+
+  function onClickHandler () {
+    props.setCount(props.count + 1);
+  }
+
+  return(
+    <button onClick={onClickHandler}>
+      counter: {props.count}
+    </button>
   )
 }
 
